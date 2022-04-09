4d121365bac40afe2677a0afd55d93f89785bdf3
marc1107 <m.bohner99@gmail.com>
2022-04-09 10:37:29 +0200

    Alle gegebenen Dateien hinzugefügt

---
+++ src/aufgabe10/TelefonBuchMenuBar.java
@@ -0,0 +1,27 @@
+// O. Bittel
+// 10.03.2017
+
+package aufgabe10;
+
+import aufgabe10.TelefonBuch;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class TelefonBuchMenuBar
+        extends JMenuBar implements ActionListener {
+
+    private TelefonBuch telBuch;
+    // ...
+
+    public TelefonBuchMenuBar(TelefonBuch tb) {
+        telBuch = tb;
+        // ...
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // ...
+    }
+}
+
